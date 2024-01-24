e2baa52a6e74e55d817602b69533f8542e23fb9a
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2024-01-24 16:18:55 +0200

    Add button to generate new grid

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-let gridSize = 32;
-
+const container = document.querySelector('#container');
 const btn = document.querySelector('#buttonResize');
+
 btn.addEventListener('click', () => {
-    gridSize = prompt("Enter square size: ", " ");
+    let gridSize;
+
+    gridSize = prompt("Enter square size: ", "16");
+    createGrid(gridSize);
 });
 
-const container = document.querySelector('#container');
-
-for (i = 0; i < gridSize ** 2; i++) {
-    const gridCell = document.createElement('div');
-    gridCell.classList.add('cell');
-    gridCell.style.flexBasis = 100 / gridSize + "%";
+function createGrid(gridSize) {
     
-    console.log(gridCell.style.flexBasis);
-    container.appendChild(gridCell);
-
-    gridCell.addEventListener('mouseenter', () => {
-        gridCell.style.backgroundColor = 'black';
-        });
-
-    gridCell.addEventListener('mouseleave', () => {
-        gridCell.style.backgroundColor = 'white';
+    const existingCells = document.querySelectorAll('.cell');
+    existingCells.forEach(cell => {
+        cell.remove();
     });
-}
 
+    for (i = 0; i < gridSize ** 2; i++) {
+        const gridCell = document.createElement('div');
+        gridCell.classList.add('cell');
+        gridCell.style.flexBasis = 100 / gridSize + "%";
+        
+        console.log(gridCell.style.flexBasis);
+        container.appendChild(gridCell);
+    
+        gridCell.addEventListener('mouseenter', () => {
+            gridCell.style.backgroundColor = 'black';
+            });
+    
+        gridCell.addEventListener('mouseleave', () => {
+            gridCell.style.backgroundColor = 'white';
+        });
+    }
+}
