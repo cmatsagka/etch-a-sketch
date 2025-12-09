dc3bd9a60d51c3ff66dcf3632761016e1d35f30d
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2025-12-09 14:52:33 +0200

    Make random color have opacity

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 
 let size = 16;
 let opacity = 100;
-let color = 'rgb(0, 0, 0, ' + opacity + '% )';
-let count = 1;
+let count = 0;
+
+let r = 0;
+let g = 0;
+let b = 0;
+let color = 'rgb(' + r + ',' + g + ',' + b + ', ' + opacity + '% )';
+let isRandomClicked = 0;
 
 const container = document.querySelector('.container');
+const btn = document.querySelector('.gridBtn');
+const randomBtn = document.querySelector('.randomBtn');
+const progDark = document.querySelector('.progDark');
 createGrid(container, size, color);
 
-const btn = document.querySelector('.gridBtn');
 btn.addEventListener('click', getGridSize);
-
-const randomBtn = document.querySelector('.randomBtn');
 randomBtn.addEventListener('click', randomizeColors);
-
-const progDark = document.querySelector('.progDark');
 progDark.addEventListener('click', getDarkerColor);
 
 function createGrid(container, size, color){
+
+
     for (let i = 0; i < size; i++){
         let gridRow = document.createElement('div');
         gridRow.classList.add('row');
@@ -53,17 +58,30 @@ function removeAllChildNodes(parent) {
 }
 
 function randomizeColors(){
-    color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ', ' + opacity + '% )'; 
+    r =  (Math.floor(Math.random() * 256));
+    g =  (Math.floor(Math.random() * 256));
+    b =  (Math.floor(Math.random() * 256));
+    color = 'rgb(' + r + ',' + g + ',' + b + ', ' + opacity + '% )'; 
     removeAllChildNodes(container);
     createGrid(container, size, color);
+    isRandomClicked = 1;
 }
 
 function getDarkerColor(){
 
     if (count == 0){
         opacity = 10;
+        count++;
     }else if (count < 10) {
         opacity += 10;
         count++;
     }
+
+    if (!isRandomClicked){
+        color = 'rgb(0, 0, 0, ' + opacity + '% )';
+    }
+
+    color = 'rgb(' + r + ',' + g + ',' + b + ', ' + opacity + '% )';
+    removeAllChildNodes(container);
+    createGrid(container, size, color);
 }
